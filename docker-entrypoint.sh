c0d22eb55f2d8b5c1fbb5ca0ad3a7c6e512ae03e
abhishek <8484451+adubey14@users.noreply.github.com>
2021-07-15 18:50:44 -0500

    several changes to install spark and update environment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ $# -eq 0 ]
   then
     /opt/conda/bin/jupyter lab --ip=0.0.0.0 --port=2222 --NotebookApp.token='local-development' --allow-root --no-browser &
-    code-server-3.11.0-linux-amd64/bin/code-server --auth none --user-data-dir /data /code --bind-addr=0.0.0.0:8443
+    code-server-3.11.0-linux-amd64/bin/code-server --auth none   --bind-addr=0.0.0.0:8443
   else
     exec "$@"
 fi
